20917231c9603de6905d2f6cefa9e10fd817b198
barbiwonderland <barbiiee.8@gmail.com>
2022-06-11 17:23:52 -0300

    styles changes

--- src/app/components/header/header/header.component.ts
+++ src/app/components/header/header/header.component.ts
@@ -1,15 +1,16 @@
+import { CartService } from './../../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  totals: any;
+  constructor(private service: CartService) {}
 
   ngOnInit(): void {
+    this.totals = this.service.getTotals().total;
   }
-
 }
